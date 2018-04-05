8578f1495d1484ea22b1ff5b1ee91f28282c87c1
Tony Mannino <immannino@Tonys-MacBook-Pro.local>
2018-04-05 02:28:48 -0700

    New favicon

--- build-deploy.sh
+++ build-deploy.sh
@@ -2,6 +2,6 @@ rm -rf docs/
 
 ng build --prod --output-path docs --base-href "https://immannino.github.io/FantasyGolfLeague/"
 
-cd ./doc/ && cp index.html ./404.html && cd ../
+cd ./docs/ && cp index.html ./404.html && cd ../
 
-git add doc/ && git commit -m "Release commit" && git push origin master
+git add docs/ && git commit -m "Release commit" && git push origin master
