7f708ac14b2d66fce739032bd23234194fec4a8e
Tony Mannino <tonymannino@me.com>
2018-04-10 00:46:10 -0700

    Added ahead of time compiler flag to build

--- build-deploy.sh
+++ build-deploy.sh
@@ -1,6 +1,6 @@
 rm -rf docs/
 
-ng build --prod --output-path docs --base-href "https://immannino.github.io/FantasyGolfLeague/"
+ng build --prod --aot --output-path docs --base-href "https://immannino.github.io/FantasyGolfLeague/"
 
 cd ./docs/ && cp index.html ./404.html && cd ../
 
